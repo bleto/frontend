84ca5edae8d4834c0e4175a046c431f69b5a7f26
Piotr Bletek <piotr.bletek@bold.net.pl>
2019-11-28 13:19:12 +0100

    ERGO-77 error 409 - support

--- plugins/axios.js
+++ plugins/axios.js
@@ -20,6 +20,7 @@ export default function ({
             auth: /401/,
             access: /403/,
             notFound: /404/,
+            conflict: /409/,
         };
 
         const { response: { data: { message }, status } } = errorResponse;
@@ -40,6 +41,9 @@ export default function ({
         case regExp.notFound.test(status):
             msg = 'Page not found';
             error({ statusCode: 404, message: msg });
+            break;
+        case regExp.conflict.test(status):
+            msg = 'Data inconsistency';
             break;
         default:
             msg = message || 'Unsupported message, please contact with support with reproduction steps';
